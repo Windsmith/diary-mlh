b159a1e85aa700108ee7420a0326f66533a7b24d
Windsmith <shriramsekar11@gmail.com>
2022-12-31 18:11:06 +0400

    feat: make entry components responsive

--- src/routes/diary/+page.svelte
+++ src/routes/diary/+page.svelte
@@ -2,28 +2,123 @@
 	let curDate = new Date();
 </script>
 
-<div class="bg-nord0 h-screen p-4">
+<div class="bg-nord0 h-screen p-4 sm:p-6">
 	<div class="flex flex-row justify-between">
 		<input
 			placeholder="Enter date to filter"
-			class="rounded bg-nord-4 text-sm text-center drop-shadow-md"
+			class="rounded bg-nord-4 text-sm sm:text-lg text-center drop-shadow-md"
 		/>
 		<a
 			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="bg-nord4 p-2 rounded text-sm active:bg-nord3 drop-shadow-md"
+			class="bg-nord4 p-2 rounded text-sm sm:text-lg sm:px-4 active:bg-nord3 drop-shadow-md"
 		>
 			Today's Entry
 		</a>
 	</div>
-	<div class="flex-col mt-8 space-y-4">
+	<div class="flex-col flex mt-8 sm:flex-row sm:flex-wrap sm:space-x-15">
 		<a
 			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="max-w-screen drop-shadow-2xl"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
 		>
-			<div class="bg-nord2 p-1 text-nord5 px-5">
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
 				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
 			</div>
-			<div class="bg-nord3 p-1 text-nord4 text-xs italic px-5">Today was a great day...</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
+		</a>
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="drop-shadow-2xl block sm:w-fit mx-6 my-4"
+		>
+			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-2xl sm:p-6 sm:px-8 sm:text-center md:px-5">
+				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
+			</div>
+			<div
+				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-lg sm:p-4 sm:px-8 md:px-5 md:text-base"
+			>
+				Today was a great day...
+			</div>
 		</a>
 	</div>
 </div>

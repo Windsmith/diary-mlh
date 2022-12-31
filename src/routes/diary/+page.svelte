ebfa688fffe2b51fd9169c623ffa390fd222a6af
Windsmith <shriramsekar11@gmail.com>
2022-12-31 12:03:57 +0400

    feat: integrate the markdown editor

---
+++ src/routes/diary/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+	import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core';
+	import { commonmark } from '@milkdown/preset-commonmark';
+	import { nord } from '@milkdown/theme-nord';
+
+	function editor(dom) {
+		Editor.make()
+			.config((ctx) => {
+				ctx.set(rootCtx, dom);
+			})
+			.use(nord)
+			.use(commonmark)
+			.create();
+	}
+</script>
+
+<div use:editor />
+
+<style></style>

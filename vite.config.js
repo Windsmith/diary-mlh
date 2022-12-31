e9e120d4b09783d4a21fec18a00ba1739a9b9b55
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-12-31 11:02:54 +0530

    fix: change yarn config instead of vite
    refer https://kit.svelte.dev/faq#does-it-work-with-yarn-2 ;
    so apparently sveltekit recommends using pnpm instead :clown:
    but this seems to work so i think we'll be fine

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	server: {
-		fs: {
-			allow: ['..']
-		}
-	},
 	plugins: [sveltekit()]
 };
 

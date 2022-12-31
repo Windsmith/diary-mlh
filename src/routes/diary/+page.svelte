f824f6a9941da793fbc21fd5f8231f7aac51b1d1
Windsmith <shriramsekar11@gmail.com>
2022-12-31 16:39:35 +0400

    feat: make hyperlink to current entry and make filter field

--- src/routes/diary/+page.svelte
+++ src/routes/diary/+page.svelte
@@ -1,1 +1,15 @@
-<h1>hi?</h1>
+<script>
+	let curDate = new Date();
+</script>
+
+<div class="bg-nord0 h-screen p-4">
+	<div class="flex flex-row justify-between">
+		<input placeholder="Enter date to filter" class="rounded bg-nord-4 text-sm text-center" />
+		<a
+			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
+			class="bg-nord4 p-2 rounded text-sm active:bg-nord3"
+		>
+			Today's Entry
+		</a>
+	</div>
+</div>

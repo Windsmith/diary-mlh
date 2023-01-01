68b0bf08309470e7bbdfcd2442aadac1d101271e
Windsmith <shriramsekar11@gmail.com>
2023-01-01 12:41:26 +0400

    feat: refactor entry component

--- src/routes/diary/+page.svelte
+++ src/routes/diary/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-  import Navbar from "../../components/navbar.svelte";
+	import Navbar from '../../components/navbar.svelte';
+	import Entry from '../../components/entry.svelte';
 	export let data;
 	let curDate = new Date();
 </script>
@@ -17,57 +18,6 @@
 		Today's Entry
 	</a>
 	<div class="flex-col flex mt-4 sm:flex-row sm:flex-wrap sm:space-x-15">
-		<a
-			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="drop-shadow-2xl block sm:w-fit mx-6 my-2"
-		>
-			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-xl sm:p-3 sm:px-8 sm:text-center md:px-5">
-				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
-			</div>
-			<div
-				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-base sm:p-2 sm:px-8 md:px-5 md:text-base"
-			>
-				Today was a great day...
-			</div>
-		</a>
-		<a
-			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="drop-shadow-2xl block sm:w-fit mx-6 my-2"
-		>
-			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-xl sm:p-3 sm:px-8 sm:text-center md:px-5">
-				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
-			</div>
-			<div
-				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-base sm:p-2 sm:px-8 md:px-5 md:text-base"
-			>
-				Today was a great day...
-			</div>
-		</a>
-		<a
-			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="drop-shadow-2xl block sm:w-fit mx-6 my-2"
-		>
-			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-xl sm:p-3 sm:px-8 sm:text-center md:px-5">
-				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
-			</div>
-			<div
-				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-base sm:p-2 sm:px-8 md:px-5 md:text-base"
-			>
-				Today was a great day...
-			</div>
-		</a>
-		<a
-			href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-			class="drop-shadow-2xl block sm:w-fit mx-6 my-2"
-		>
-			<div class="bg-nord2 p-1 text-nord5 px-5 sm:text-xl sm:p-3 sm:px-8 sm:text-center md:px-5">
-				{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}
-			</div>
-			<div
-				class="bg-nord3 p-1 text-nord4 text-xs italic px-6 sm:text-base sm:p-2 sm:px-8 md:px-5 md:text-base"
-			>
-				Today was a great day...
-			</div>
-		</a>
+		<Entry />
 	</div>
 </div>

f2e345d6becc6705b908e8bb704566c892e67cff
Windsmith <shriramsekar11@gmail.com>
2023-01-01 12:54:36 +0400

    tiny design changes

--- src/routes/diary/+page.svelte
+++ src/routes/diary/+page.svelte
@@ -6,14 +6,14 @@
 </script>
 
 <Navbar />
-<div class="bg-nord0 h-screen p-4 sm:p-6">
+<div class="bg-nord4 h-screen p-4 sm:p-6">
 	<input
 		placeholder="Enter date to filter"
 		class="rounded bg-nord-4 text-base p-1 m-auto flex sm:text-lg text-center drop-shadow-md"
 	/>
 	<a
 		href="/diary/{curDate.getFullYear()}-{curDate.getMonth() + 1}-{curDate.getDate()}"
-		class="bg-nord4 p-4 m-4 rounded text-xl sm:text-3xl sm:p-8 md:py-12 active:bg-nord3 drop-shadow-md block text-center"
+		class="bg-nord14 p-4 m-4 rounded text-xl text-nord1 sm:text-3xl sm:p-8 md:py-12 drop-shadow-md block text-center"
 	>
 		Today's Entry
 	</a>
